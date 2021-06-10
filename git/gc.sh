8c1ea5981a25768921fbf03783bd3d94065bd342
xpwosr <x1npen9w@gmail.com>
2021-06-10 11:32:26 +0800

    solve folder issue

--- git/gc.sh
+++ git/gc.sh
@@ -12,18 +12,29 @@ function check_result() {
   fi
 }
 
-MSG=$(git clone "${URL}")
-check_result $? ${MSG}
+echo "Cloning ${URL} ..."
+MSG=`git clone "${URL}" 2>&1`
+check_result $? "${MSG}"
+REPO_FOLDER=$(echo "${MSG}" | awk '{split($0,s," "); print s[3]}' | sed "s/[\'|\.]//g")
+echo "Repository is cloned into ./${REPO_FOLDER}"
 
-read -p 'user.name: ' username
-read -p 'user.email: ' useremail
+if [ -d "./${REPO_FOLDER}" ]; then
+    cd "./${REPO_FOLDER}"
+    git status
 
-echo "Please Confirm:"
-echo " user.name: ${username}"
-echo " user.email: ${useremail}"
+    read -p 'user.name: ' username
+    read -p 'user.email: ' useremail
 
-read -p "Only 'yes' for confirm: " confirm
-if [ "${confirm}" == "yes" ]; then
-  git config --local user.name "${username}"
-  git config --local user.email "${useremail}"
+    echo "\nPlease Confirm:"
+    echo "=========================================="
+    echo " user.name: ${username}"
+    echo " user.email: ${useremail}"
+    echo "=========================================="
+    read -p "Only 'yes' for confirm: " confirm
+
+    if [ "${confirm}" == "yes" ]; then
+      git config --local user.name "${username}"
+      git config --local user.email "${useremail}"
+    fi
 fi
+
