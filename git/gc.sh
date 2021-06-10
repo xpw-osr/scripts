49177d27709d4440a0a56ca6ce3de35f9a55ada1
xpwosr <x1npen9w@gmail.com>
2021-06-10 10:32:06 +0800

    add script for clone and set user.name/user.email for local

---
+++ git/gc.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env sh
+
+URL=$1
+
+function check_result() {
+  RET=$1
+  MSG=$2
+
+  if [ ${RET} -ne 0 ]; then
+    echo ${MSG}
+    exit ${RET}
+  fi
+}
+
+# MSG=$(git clone "${URL}")
+# check_result $? ${MSG}
+echo "Mock clone repo from git"
+
+read -p 'user.name: ' username
+read -p 'user.email: ' useremail
+
+echo "Please Confirm:"
+echo " user.name: ${username}"
+echo " user.email: ${useremail}"
+
+read -p "Only 'yes' for confirm: " confirm
+if [ "${confirm}" == "yes" ]; then
+  git config --local user.name "${username}"
+  git config --local user.email "${useremail}"
+fi
