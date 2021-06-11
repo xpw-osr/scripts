9774a5d9a3260aba738a1fe31eb0f4d86a0f48d7
xpwosr <x1npen9w@gmail.com>
2021-06-11 10:09:36 +0800

    add README; refactor git/gc.sh for messages

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # scripts
+
+## Commands
+
+### gc
+Git clone and set local user.name and user.email
+```shell
+$ gc <git-uri>
+```
+
+## Deployment
+* Clone repo to local
+    ```shell
+    $ cd /path/to/your/localtion
+    $ git clone git@github.com:xpw-osr/scripts.git
+    ```
+    and add it to path
+    ```shell
+    export SCRIPTS_HOME=/path/to/scripts/repo/folder
+    ```
+* Add following code to your shell rc file
+    ```shell
+    alias gc=$SCRIPTS_HOME/git/gc.sh 
+    ```
--- git/gc.sh
+++ git/gc.sh
@@ -12,6 +12,16 @@ function check_result() {
   fi
 }
 
+function show_messages() {
+    TITLE=$1
+    CONTENT=$2
+
+    echo "\n${TITLE}"
+    echo "=========================================="
+    echo "${CONTENT}"
+    echo "=========================================="
+}
+
 echo "Cloning ${URL} ..."
 MSG=`git clone "${URL}" 2>&1`
 check_result $? "${MSG}"
@@ -20,23 +30,26 @@ echo "Repository is cloned into ./${REPO_FOLDER}"
 
 if [ -d "./${REPO_FOLDER}" ]; then
     cd "./${REPO_FOLDER}"
-    git status
+    show_messages "Repository information" "$(git status)"
 
-    echo "\nSet Local Account"
+    echo "\nEnter user information"
     echo "=========================================="
     read -p 'user.name: ' username
     read -p 'user.email: ' useremail
 
-    echo "\nPlease Confirm:"
-    echo "=========================================="
-    echo " user.name: ${username}"
-    echo " user.email: ${useremail}"
-    echo "=========================================="
+    show_messages "Please Confirm:" "user.name: ${username}\nuser.email: ${useremail}"
     read -p "Only 'yes' for confirm: " confirm
 
     if [ "${confirm}" == "yes" ]; then
-      git config --local user.name "${username}"
-      git config --local user.email "${useremail}"
+        git config --local user.name "${username}"
+        git config --local user.email "${useremail}"
+
+        RESULTS=$(git config --list | grep 'user.')
+        if [ "${RESULTS}" == "" ]; then
+            show_messages "Failed to add user info to configurations" "${RESULTS}"
+            exit 1
+        fi
+        show_messages "Following configurations have been added:" "${RESULTS}"
     fi
 fi
 
