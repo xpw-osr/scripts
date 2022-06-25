79e887ad98e1b13ec41eb43377b71f7ce27a9d45
xinpeng.wang <xinpeng.wang@thoughtworks.com>
2022-06-25 21:30:17 +0800

    implement generic algorithm for recursing submodules

--- git/gcs.sh
+++ git/gcs.sh
@@ -9,19 +9,29 @@
 #     |  +- ...
 #     +- ...
 # ---------------------------------------------------------
+
+recurse_submodules() {
+    SUBMODULE_FOLDER="$1"
+    cd "${SUBMODULE_FOLDER}"
+
+    git submodule update --init
+    git pull origin main
+
+    git submodule | awk '{print $2}' | while read submodule; do
+        recurse_submodules "${submodule}"
+    done
+}
+
 REPO_URL="$1"
 PROJECT_ROOT="$2"
 
 if test -z "${PROJECT_ROOT}"; then
-    PROJECT_ROOT=`git clone --recurse-submodules ${REPO_URL} ${PROJECT_ROOT} 2>&1 | tee /dev/tty | grep -o -P "(?<=Cloning into ').*(?='...)" | grep "^[^/]"`
+    PROJECT_ROOT=`git clone ${REPO_URL} ${PROJECT_ROOT} 2>&1 | tee /dev/tty | grep -o -P "(?<=Cloning into ').*(?='...)" | grep "^[^/]"`
 else
-    git clone --recurse-submodules ${REPO_URL} ${PROJECT_ROOT}
+    git clone ${REPO_URL} ${PROJECT_ROOT}
 fi
 
 cd ${PROJECT_ROOT}
-git submodule foreach "
-    git pull origin main --recurse-submodules
-    git submodule | while read module; do
-        git submodule update --init --recursive --  $(echo module | awk '{print $2}')
-    done
-"
+git submodule | awk '{print $2}' | while read submodule; do
+    recurse_submodules ${submodule}
+done
